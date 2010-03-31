05ff1dd852a2c9640ff664c242a1005073a8eb03
grahamjones139 <grahamjones139@mycompany.com>
2010-03-31 21:41:50 +0000

    added setpermissions.sql
    git-svn-id: https://townguide.googlecode.com/svn/trunk@67 7f0aec4c-f96d-11de-8c8a-61c5e9addfc9

---
+++ setpermissions.sql
@@ -0,0 +1,4 @@
+grant all privileges on planet_osm_nodes to www;
+grant all privileges on planet_osm_point to www;
+grant all privileges on planet_osm_rels to www;
+grant all privileges on planet_osm_ways to www;
