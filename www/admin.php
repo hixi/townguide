7b44599eae1de68f3006724970b4b4c12dc4b35a
grahamjones139 <grahamjones139@mycompany.com>
2010-10-15 19:34:53 +0000

    added re-try option to admin page to re-queue a job.
    git-svn-id: https://townguide.googlecode.com/svn/trunk@145 7f0aec4c-f96d-11de-8c8a-61c5e9addfc9

--- www/admin.php
+++ www/admin.php
@@ -1,12 +1,29 @@
 <?php include("header.php");?>
 
 <?php
-if (!isset($_POST['deleteSelected'])) {
+if (isset($_POST['deleteSelected'])) {
+   echo "<h3>Deleting</h3>";
+   $delList = $_POST['delList'];
 
-   echo "<h1>Townguide Queue Administration</h2>\n";
-   print "<h3>Select Jobs to Delete </h3>";
+   foreach ($delList as $jobNo) {
+   	   echo "$jobNo is selected";    
+     	   deleteJob($jobNo);
+	   }    
+}
+elseif (isset($_POST['retrySelected'])) {
+       echo "<h3>Re-Queueing</h3>";
+       $delList = $_POST['delList'];
 
-   $dbconn = pg_connect("host=localhost dbname=townguide user=www password=1234")
+       foreach ($delList as $jobNo) {
+       	       echo "$jobNo is selected";    
+     	       retryJob($jobNo);
+	       }
+}
+else {
+     echo "<h1>Townguide Queue Administration</h2>\n";
+     print "<h3>Select Jobs to Delete </h3>";
+
+     $dbconn = pg_connect("host=localhost dbname=townguide user=www password=1234")
      or die('Could not connect: ' . pg_last_error());
 
      $query = 'SELECT jobno,status,title FROM queue order by jobno desc';
@@ -16,8 +33,9 @@ if (!isset($_POST['deleteSelected'])) {
 
      echo "<form action='admin.php' method='POST'>";
      echo "<input type='submit' value='Delete Selected Jobs' name='deleteSelected'>";
+     echo "<input type='submit' value='Retry Selected Jobs' name='retrySelected'>";
      echo "<table border='1'>\n";
-     echo "<tr><th>JobNo</th><th>Status</th><th>Title</th><th>Delete?</th>";
+     echo "<tr><th>JobNo</th><th>Status</th><th>Title</th><th>Select</th>";
      $resultCount = 0;	
      while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
            $resultCount = $resultCount + 1;
@@ -40,23 +58,27 @@ if (!isset($_POST['deleteSelected'])) {
                 echo "<td>Unknown status ",$line['status'],"</td>";
     	   }
     
-    echo "\t\t<td><a href='output/",$line['jobno'],"'>",$line['title'],"</a></td>\n";
-    echo "\t\t<td><input type=checkbox name='delList[]' value='",$line['jobno'],"'></td>\n";
-    echo "\t</tr>\n";
-    }
+	echo "\t\t<td><a href='output/",$line['jobno'],"'>",$line['title'],"</a></td>\n";
+    	echo "\t\t<td><input type=checkbox name='delList[]' value='",$line['jobno'],"'></td>\n";
+    	echo "\t</tr>\n";
+    	}
 	echo "</table>\n";
-    echo "</form>";
-}
-else {
-     echo "<h3>Deleting</h3>";
-     $delList = $_POST['delList'];
+    	echo "</form>";
+ }
 
-     foreach ($delList as $jobNo) 
-     {
-     echo "$jobNo is selected";    
-     deleteJob($jobNo);
-     }
 
+
+function retryJob($jobNo)
+{
+	echo "<p>retryJob($jobNo)</p>";
+
+   	$dbconn = pg_connect("host=localhost dbname=townguide user=www password=1234")
+     	or die('Could not connect: ' . pg_last_error());
+
+     	$query = 'update queue set status=0 where jobno='.$jobNo;
+     	$result = pg_exec($query) or die('Query failed: ' . pg_last_error());
+
+	echo "<p><a href='admin.php'>Return to Admin Page</a></p>\n";
 }
 
 
@@ -72,7 +94,7 @@ function deleteJob($jobNo)
      	$result = pg_exec($query) or die('Query failed: ' . pg_last_error());
 	deleteDirectory($dir);
 
-	echo "<p><a href='admin.php>Return to Admin Page</a></p>\n";
+	echo "<p><a href='admin.php'>Return to Admin Page</a></p>\n";
 }
 
 function deleteDirectory($dir)
